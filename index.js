0981c0ff2355ed752dee2300777548b02d49cccf
vishnu <vishnusaran37@gmail.com>
2020-01-29 17:15:53 +0800

    create db connection

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const port = 3006;
+const db = require('./db/connection');
+
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json());
+
+try {
+    db.authenticate();
+    console.log('db connected successfully !!')
+}
+catch (error) {
+    console.log('there is an error', error);
+}
 
 app.get('/', (req, res) => {
     res.send('Welcome to Assignment');
