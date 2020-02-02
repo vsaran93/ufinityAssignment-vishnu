0d35f55b9df09333f0d6ddfdfdc1d8d613fbf81f
vishnu <vishnusaran37@gmail.com>
2020-02-02 19:59:49 +0800

    resister modal api wireup for single teacher and single student

--- student-management/src/components/teacher/RegisterModal.js
+++ student-management/src/components/teacher/RegisterModal.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, Fragment } from 'react';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
@@ -13,11 +13,14 @@ import '../../index.css';
 
 const ERROR_MSG = "To Register Teacher's Email address and at least one student email address needed!";
 const TITLE = "Register Teacher and Students";
+
 export default function ResponsiveDialog(props) {
     const [students, setStudents] = useState([]);
     const [teacher, setTeacher] = useState("");
     const [message, setMessage] = useState("");
     const [primaryStudent, setPrimaryStudent] = useState("");
+    const [registerSuccess, setRegisterSuccess] = useState(false);
+    const [successMsg, setSuccessMsg] = useState("")
     const theme = useTheme();
     const fullScreen = useMediaQuery(theme.breakpoints.down('sm'));
     const addInput = ev => {
@@ -29,16 +32,35 @@ export default function ResponsiveDialog(props) {
         setFunction(e.target.value)
     }
 
+    const handleCancel = () => {
+        setMessage("");
+        setSuccessMsg("");
+        setTeacher("");
+        setPrimaryStudent("");
+        props.closeModal();
+    }
+
     const handleRegister = () => {
-        if(primaryStudent && teacher) {
+        if (primaryStudent && teacher) {
             let studentList = [];
             studentList.push(primaryStudent)
-            register({"teacher": teacher, "Students": studentList}).then((res) => {
-                
+            register({ "teacher": teacher, "students": studentList }).then((res) => {
+                setRegisterSuccess(true);
+                setSuccessMsg(res.data.message);
             })
         } else {
             setMessage(ERROR_MSG);
         }
+    }
+    const displaySuccessDiv = (msg) => {
+        return (
+            <div className="success-div">
+                <p className="success-msg">{msg}</p>
+                <Button onClick={handleCancel} variant="outlined" color="primary" >
+                    Ok
+                </Button>
+            </div>
+        )
     }
     return (
         <div>
@@ -50,42 +72,45 @@ export default function ResponsiveDialog(props) {
             >
                 <DialogTitle id="responsive-dialog-title">{TITLE}</DialogTitle>
                 <div><p className="error-msg">{message}</p></div>
-                <DialogContent>
-                    <Grid container spacing={3}>
-                        <Grid item xs={12} sm={6}>
-                            <div><h4>Teacher</h4></div>
-                            <TextField
-                                value={teacher}
-                                label="Email Address"
-                                onChange={(e) => handleInput(e,setTeacher)}
-                                required
-                                id="outlined-required"
-                                variant="outlined"
-                            />
-                        </Grid>
-                        <Grid item xs={12} sm={6}>
-                            <div><h4>Students</h4></div>
-                            <TextField
-                                value={''}
-                                label="Email Address"
-                                onChange={(e) => handleInput(e, setPrimaryStudent)}
-                                required
-                                id="outlined-required"
-                                variant="outlined"
-                            />
-                            <div><Button onClick={addInput} color="primary" autoFocus>Add Student</Button></div>
-                            {students.map(a => <TextField className="student-input" required id="outlined-required" variant="outlined" label="Email Address" />)}
-                        </Grid>
-                    </Grid>
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={props.closeModal} variant="outlined" color="secondary" >
-                        Cancel
+                {registerSuccess ? displaySuccessDiv(successMsg) :
+                    <Fragment>
+                        <DialogContent>
+                            <Grid container spacing={3}>
+                                <Grid item xs={12} sm={6}>
+                                    <div><h4>Teacher</h4></div>
+                                    <TextField
+                                        value={teacher}
+                                        label="Email Address"
+                                        onChange={(e) => handleInput(e, setTeacher)}
+                                        required
+                                        id="outlined-required"
+                                        variant="outlined"
+                                    />
+                                </Grid>
+                                <Grid item xs={12} sm={6}>
+                                    <div><h4>Students</h4></div>
+                                    <TextField
+                                        value={primaryStudent}
+                                        label="Email Address"
+                                        onChange={(e) => handleInput(e, setPrimaryStudent)}
+                                        required
+                                        id="outlined-required"
+                                        variant="outlined"
+                                    />
+                                    <div><Button onClick={addInput} color="primary" autoFocus>Add Student</Button></div>
+                                    {students.map(a => <TextField className="student-input" required id="outlined-required" variant="outlined" label="Email Address" />)}
+                                </Grid>
+                            </Grid>
+                        </DialogContent>
+                        <DialogActions>
+                            <Button onClick={handleCancel} variant="outlined" color="secondary" >
+                                Cancel
                     </Button>
-                    <Button onClick={handleRegister} variant="outlined" color="primary">
-                        Register
+                            <Button onClick={handleRegister} variant="outlined" color="primary">
+                                Register
                     </Button>
-                </DialogActions>
+                        </DialogActions>
+                    </Fragment>}
             </Dialog>
         </div>
     );
